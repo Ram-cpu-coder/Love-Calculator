844c874a97da4ca09caa6b911dabb37be486820a
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-09 11:47:10 +1100

    structuring

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const user_input = document.querySelector("#user-input");
 const partner_input = document.querySelector("#partner-input");
+const input = document.querySelectorAll(".input");
+console.log(input);
 
 const calculate_button = document.querySelector(".button");
-let resultToDisplay = "";
+let resultToDisplay = 0;
 const vowels = "aeiou";
 const love = "love";
-const consonants = "bcdfghjklmnpqrstvwxyz"
+const consonants = "bcdfghjklmnpqrstvwxyz";
 const calculate = ()=>{
     if()
+}
+calculate_button.addEventListener("click",()=>{
+    calculate();
+    display(resultToDisplay);
+})
+const display = ()=>{
+    console.log(resultToDisplay);
+    
 }
