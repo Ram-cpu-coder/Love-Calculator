03fac38b079d63bff56cf3e2f389d87d734b1dfa
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-09 11:42:47 +1100

    trying in js

---
+++ script.js
@@ -0,0 +1,11 @@
+const user_input = document.querySelector("#user-input");
+const partner_input = document.querySelector("#partner-input");
+
+const calculate_button = document.querySelector(".button");
+let resultToDisplay = "";
+const vowels = "aeiou";
+const love = "love";
+const consonants = "bcdfghjklmnpqrstvwxyz"
+const calculate = ()=>{
+    if()
+}
