cd33a0c67c63ba2a296963532dde9eb6e40a1b51
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-09 15:47:05 +1100

    on progress on the condition

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const user_input = document.querySelector("#user-input");
 const partner_input = document.querySelector("#partner-input");
 const input = document.querySelectorAll(".input");
-console.log(input);
+// console.log(input);
 
 const calculate_button = document.querySelector(".button");
-let resultToDisplay = 0;
 const vowels = "aeiou";
 const love = "love";
 const consonants = "bcdfghjklmnpqrstvwxyz";
-const calculate = ()=>{
-    if()
-}
-calculate_button.addEventListener("click",()=>{
-    calculate();
-    display(resultToDisplay);
-})
-const display = ()=>{
-    console.log(resultToDisplay);
-    
-}
+const calculate = () => {
+  let resultToDisplay = 0;
+  if (user_input.value.length === partner_input.value.length) {
+    resultToDisplay += 20;
+  }
+  if (hasVowels(user_input.value) && hasVowels(partner_input.value)) {
+    resultToDisplay += 10;
+  }
+
+  display(resultToDisplay);
+};
+calculate_button.addEventListener("click", () => {
+  calculate();
+});
+const display = (str) => {
+  console.log(str);
+};
+
+const hasVowels = (input) => {
+  for (let item of vowels) {
+    if (input.includes(item)) {
+      return true;
+    }
+  }
+  return false;
+};
